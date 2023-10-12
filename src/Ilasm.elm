8c34793ba1bf06739074a6663db70fa62da1e29b
sreguera <sreguera@gmail.com>
2023-10-12 21:41:19 +0200

    Assemble symbolic labels

--- src/Ilasm.elm
+++ src/Ilasm.elm
@@ -1,8 +1,9 @@
 module Ilasm exposing (..)
 
 import Ilvm
+import Dict exposing (Dict)
 
-type alias Label = Int
+type alias Label = String
 
 type Inst 
     = TST Label String
@@ -43,21 +44,45 @@ type Inst
 
 assemble : List Inst -> List Ilvm.Opcode
 assemble insts =
-    List.filterMap assemble1 insts
+    pass2 insts (pass1 insts)
 
-assemble1 : Inst -> Maybe Ilvm.Opcode
-assemble1 inst =
+type alias SymTab = Dict String Int
+
+pass1 : List Inst -> SymTab
+pass1 program =
+    let
+        pass1Int : List Inst -> Int -> SymTab -> SymTab
+        pass1Int insts offset syms =
+            case insts of
+                LABEL l :: rest ->
+                    pass1Int rest offset (Dict.insert l offset syms)
+                _ :: rest ->
+                    pass1Int rest (offset + 1) syms
+                [] ->
+                    syms
+    in
+    pass1Int program 0 Dict.empty
+
+pass2 : List Inst -> SymTab -> List Ilvm.Opcode
+pass2 insts syms =
+    List.filterMap (assemble1 syms) insts
+
+assemble1 : SymTab -> Inst -> Maybe Ilvm.Opcode
+assemble1 syms inst =
+    let
+        lookup name = Maybe.withDefault 0 (Dict.get name syms)
+    in
     case inst of
         TST label string ->
-            Just <| Ilvm.TST label string
+            Just <| Ilvm.TST (lookup label) string
         CALL label ->
-            Just <| Ilvm.CALL label
+            Just <| Ilvm.CALL (lookup label)
         RTN ->
             Just <| Ilvm.RTN
         DONE ->
             Just <| Ilvm.DONE
         JMP label ->
-            Just <| Ilvm.JMP label
+            Just <| Ilvm.JMP (lookup label)
         PRS ->
             Just <| Ilvm.PRS
         PRN ->
@@ -99,9 +124,9 @@ assemble1 inst =
         STORE ->
             Just <| Ilvm.STORE
         TSTV label ->
-            Just <| Ilvm.TSTV label
+            Just <| Ilvm.TSTV (lookup label)
         TSTN label ->
-            Just <| Ilvm.TSTN label
+            Just <| Ilvm.TSTN (lookup label)
         IND ->
             Just <| Ilvm.IND
         LST ->
@@ -111,7 +136,7 @@ assemble1 inst =
         GETLINE ->
             Just <| Ilvm.GETLINE
         TSTL label ->
-            Just <| Ilvm.TSTL label
+            Just <| Ilvm.TSTL (lookup label)
         INSRT ->
             Just <| Ilvm.INSRT
         XINIT ->
