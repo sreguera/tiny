6172a96191f5ffd07af064021766ebed18f30238
sreguera <sreguera@gmail.com>
2023-10-18 22:07:13 +0200

    UI: Show ucode, vars, lines

--- src/Main.elm
+++ src/Main.elm
@@ -3,11 +3,15 @@ module Main exposing (..)
 -- Tiny Basic interpreter.
 
 import Browser
-import Element exposing (Element)
+import Element exposing (Element, fill, rgb)
 import Element.Input
+import Element.Font as Font
+import Element.Background as Background
 import Html exposing (Html)
 import Ilvm exposing (VM, resume, resumeWithInput)
 import Interp
+import Dict
+import Array
 
 -- MAIN
 
@@ -65,12 +69,19 @@ viewModel : Model -> Element Msg
 viewModel model =
     Element.column []
         [ Element.text "Tiny Basic"
-        , viewTerminal model
+        , Element.row []
+            [ Element.column [ Element.width fill, Element.alignTop ]
+                [ viewTerminal model
+                , viewLines model
+                , viewVars model
+                ]
+            , viewUcode model
+            ]
         ]
 
 viewTerminal : Model -> Element Msg
 viewTerminal model =
-    Element.column []
+    Element.column [ Font.family [Font.monospace] ]
         [ viewLog model.log
         , Element.Input.text [] 
             { onChange = GotInput
@@ -88,3 +99,59 @@ viewLog : List String -> Element Msg
 viewLog log =
     Element.column []
         (List.map Element.text (List.reverse log))
+
+viewLines : Model -> Element Msg
+viewLines model =
+    Element.table [ Font.family [Font.monospace] ]
+        { data = Dict.toList model.vm.lines
+        , columns =
+            [ { header = Element.text "Lineno"
+              , width = Element.fill
+              , view = \(lineno, _) -> Element.text (String.fromInt lineno)
+              }
+            , { header = Element.text "Code"
+              , width = Element.fill
+              , view = \(_, code) -> Element.text code
+              }
+            ]
+        }
+
+viewVars : Model -> Element Msg
+viewVars model =
+    Element.table [ Font.family [Font.monospace] ]
+        { data = Dict.toList model.vm.vars
+        , columns =
+            [ { header = Element.text "Var"
+              , width = Element.fill
+              , view = \(var, _) -> Element.text (String.fromChar (Char.fromCode (var + Char.toCode 'A')))
+              }
+            , { header = Element.text "Value"
+              , width = Element.fill
+              , view = \(_, value) -> Element.text (String.fromInt value)
+              }
+            ]
+        }
+
+viewUcode : Model -> Element Msg
+viewUcode model =
+    let
+        showCurrent address elem =
+            if address == model.vm.pc then
+                Element.el [ Background.color (rgb 0.7 0.7 0.9) ] elem
+            else
+                elem
+    in
+    Element.table [ Font.family [Font.monospace], Element.scrollbarY, Element.height (Element.px 500) ]
+        { data = Array.toIndexedList model.vm.code
+        , columns =
+            [ { header = Element.text "Address"
+              , width = Element.fill
+              , view = \(address, _) -> showCurrent address <| Element.text (String.fromInt address)
+              }
+            , { header = Element.text "Opcode"
+              , width = Element.fill
+              , view = \(address, opcode) -> showCurrent address <| Element.text (Debug.toString opcode)
+              }
+            ]
+        }
+
